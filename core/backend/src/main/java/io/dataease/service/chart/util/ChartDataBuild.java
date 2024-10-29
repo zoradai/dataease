80730f7599bdfc45a64f2e3d41b221a477501e79
taojinlong <jinlong@fit2cloud.com>
2024-10-29 15:26:13 +0800

    fix: 修复格式化数据出错

--- core/backend/src/main/java/io/dataease/service/chart/util/ChartDataBuild.java
+++ core/backend/src/main/java/io/dataease/service/chart/util/ChartDataBuild.java
@@ -1194,7 +1194,12 @@ public class ChartDataBuild {
                 if (chartViewFieldDTO.getDeType() == 0 || chartViewFieldDTO.getDeType() == 1 || chartViewFieldDTO.getDeType() == 5) {
                     d.put(fields.get(i).getDataeaseName(), StringUtils.isEmpty(ele[i]) ? "" : ele[i]);
                 } else if (chartViewFieldDTO.getDeType() == 2 || chartViewFieldDTO.getDeType() == 3) {
-                    d.put(fields.get(i).getDataeaseName(), StringUtils.isEmpty(ele[i]) ? null : new BigDecimal(ele[i]).setScale(8, RoundingMode.HALF_UP));
+                    try {
+                        d.put(fields.get(i).getDataeaseName(), StringUtils.isEmpty(ele[i]) ? null : new BigDecimal(ele[i]).setScale(8, RoundingMode.HALF_UP));
+                    } catch (NumberFormatException e) {
+                        d.put(fields.get(i).getDataeaseName(), StringUtils.isEmpty(ele[i]) ? "" : ele[i]);
+                    }
+
                 }
             }
             tableRow.add(d);
